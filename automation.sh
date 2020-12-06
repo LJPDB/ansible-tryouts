33d0852168d631ece12631922ac1d78d4486bc6d
Leonardo Puga <leonardopuga@FVFC427UL414.home>
2020-12-07 00:35:23 +0100

    adding ips array to go through and execute

--- automation.sh
+++ automation.sh
@@ -3,9 +3,16 @@
 export ID_RSA_NAME="id_automation"
 export ID_RSA_PASS="Qwerty01."
 
-SSHKEYGEN="yes | ssh-keygen -t rsa -f ~/.ssh/$ID_RSA_NAME -q -P \"$ID_RSA_PASS\""
+KEYPATH="$PWD/automation/.ssh"
+mkdir -p $KEYPATH
 
-
+SSHKEYGEN="yes | ssh-keygen -t rsa -f $KEYPATH/$ID_RSA_NAME -q -P \"$ID_RSA_PASS\""
+HOST_IPS=("188.166.24.188" "174.138.8.187")
+for HOST_IP in "${HOST_IPS[@]}"
+do
+    echo $HOST_IP
+    #eval "ssh-copy-id -i $KEYPATH/$ID_RSA_NAME.pub  ansible@$HOST_IP"
+done
 
 echo $SSHKEYGEN
 
